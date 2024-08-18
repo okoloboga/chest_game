171815769b922eb61631cec25aae3af8701d7be9
user <okoloboga37@gamil.com>
2024-08-18 15:31:05 +0300

    telegraph text link added

--- services/constants.py
+++ services/constants.py
@@ -1,5 +1,5 @@
 CHANNEL = 't.me/test_channel_okoloboga'
 DOG_CHANNEL = '@test_channel_okoloboga'
-TELEGRAPH = 'https://telegra.ph/KATALOG-TOVAROV-01-18'
+TELEGRAPH = 'https://telegra.ph/Example-08-18-2'
 # CENTRAL_WALLET = 'UQDIkS1d_Lhd7EDttTtcmr9Xzg78uEMDEsYFde-PZCgfoOtU'
 CENTRAL_WALLET = '0QDIkS1d_Lhd7EDttTtcmr9Xzg78uEMDEsYFde-PZCgfoFDe'
